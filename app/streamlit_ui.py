3aa728154ce988f45b6183bcf84e82be1b3a027b
pingu9 <h1h2h1@naver.com>
2024-08-18 14:11:54 +0900

    이전 대화 기록 저장하도록 변경

--- app/streamlit_ui.py
+++ app/streamlit_ui.py
@@ -2,6 +2,7 @@ import streamlit as st
 import requests
 
 from langchain_core.messages.chat import ChatMessage
+from langchain_community.chat_message_histories import ChatMessageHistory
 
 from models import ModelManager
 
@@ -12,17 +13,23 @@ st.title("금융 마이데이터 Q&A 에이전트")
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-def print_message(message):
+def print_messages():
     for chat_message in st.session_state.messages:
         st.chat_message(chat_message.role).write(chat_message.content)
 
 def add_message(role, message):
     st.session_state.messages.append(ChatMessage(role=role, content=message))
 
+def clear_messages():
+    st.session_state.messages = []
+    print_messages()
+
+print_messages()
 
 question = st.chat_input("질문을 입력하세요:")
 
 if question:
+    st.chat_message("user").write(question)
     answer_stream = model_manager.generate_answer_stream(question)
     with st.chat_message("assistant"):
         container = st.empty()
@@ -30,5 +37,9 @@ if question:
         for token in answer_stream:
             answer += token
             container.markdown(answer)
+        add_message("user", question)
+        add_message("assistant", answer)
 
-    add_message("user", question)
+
+if st.button("대화 초기화"):
+    clear_messages()
